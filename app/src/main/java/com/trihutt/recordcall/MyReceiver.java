7dee2defd4d25dc9a5c3bd158c7c6eb2ec20a8cf
surajc414 <53199894+surajc414@users.noreply.github.com>
2019-11-06 01:07:58 +0530

    Initial commit

---
+++ app/src/main/java/com/trihutt/recordcall/MyReceiver.java
@@ -0,0 +1,34 @@
+package com.trihutt.recordcall;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.FileDescriptor;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    String AudioSavePathInDevice=null;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context, "Power Disconnected", Toast.LENGTH_SHORT).show();
+        Log.e("Msg","Power Disconnected");
+
+        AudioSavePathInDevice=Environment.getDownloadCacheDirectory().getAbsoluteFile()+"/"+"abc.3gp";
+        MediaRecorder mr = new MediaRecorder();
+        mr.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mr.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mr.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB);
+        mr.setOutputFile(AudioSavePathInDevice);
+
+        throw new UnsupportedOperationException("Not yet implemented");
+        
+        
+    }
+}
